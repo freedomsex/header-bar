6773f8ca9c4291e6969528e194204b722a62a9f7
tebaly <support@freedomsex.net>
2020-08-31 23:02:35 +0300

    feat: header button component

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,13 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [1.0.4](https://github.com/freedomsex/header-bar/compare/v1.0.3...v1.0.4) (2020-08-31)
+
+
+### Bug Fixes
+
+* name ([69febb7](https://github.com/freedomsex/header-bar/commit/69febb7141b8d598d03a67479d6a8383dd3b0f48))
+
 ### [1.0.3](https://github.com/freedomsex/header-bar/compare/v1.0.1...v1.0.3) (2020-08-31)
 
 
---
+++ HeaderButton.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: ['icon', 'title', 'limit'],
+};
+</script>
+
+<template>
+  <div class="header-bar__button" @click="$emit('click')">
+    <span class="header-bar__title" :class="{ limit }" v-if="title">
+      {{title}}
+    </span>
+    <i class="material-icons" v-if="icon">
+      {{icon}}
+    </i>
+  </div>
+</template>
+
+<style lang="less">
+.header-bar {  
+  &__button {
+    font-size: 0;
+    display: flex;
+    align-items: center;
+    padding: 0px 5px;
+    overflow: hidden;
+    white-space: nowrap;
+    border-radius: 3px;
+    cursor: pointer;
+    text-decoration: none;
+    position: relative;
+
+    &.caption {
+      padding-left: 0px;
+    }
+
+    a {
+      color: #ffffff;
+      &:hover {
+        text-decoration: none;
+      }
+    }
+  }
+
+  &__title {
+    font-size: 16px;
+    padding-top: 2px;
+    margin: 0px 5px;
+    position: relative; 
+    overflow: hidden;
+    white-space: nowrap; 
+
+    &.limit {
+      max-width: 150px;
+    }
+  } 
+}
+</style>
